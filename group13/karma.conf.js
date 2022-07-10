bfe8b6376703468758d2d87c58d65cacf8b5c601
madhav97 <hulikuntemadhav@gmail.com>
2022-07-10 12:43:11 +0530

    added e2e

--- group13/karma.conf.js
+++ group13/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/group13'),
+      dir: require('path').join(__dirname, './coverage/shop-for-home'),
       subdir: '.',
       reporters: [
         { type: 'html' },
